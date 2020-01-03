3c50505863804b61a63a9bc35c61418a4dc3bf6d
kwaneung <42456925+kwaneung@users.noreply.github.com>
2020-01-03 09:56:58 +0900

    .

---
+++ Codingtest/191125.py
@@ -0,0 +1,26 @@
+# 정수 배열(int array)이 주어지면 두번째로 큰 값을 프린트하시오.
+#
+# Given an integer array, find the second largest element.
+#
+# 예제)
+# Input: [10, 5, 4, 3, -1]
+# Output: 5
+#
+# Input: [3, 3, 3]
+# Output: Does not exist.
+
+
+def ret_second(lst):
+    lst = list(set(lst))  # set 함수로 중복 제거
+    lst.sort(reverse=True)  # sort 함수로 정렬, 내림차순
+    if len(lst) < 2:  # 두번쨰로 큰 함수가 존재하지 않으면
+        print("Does not exist")
+    else:
+        print(lst[1])
+
+
+if __name__ == "__main__":
+    a = [10, 5, 4, 3, -1]
+    b = [3, 3, 3]
+    ret_second(a)
+    ret_second(b)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Coding-Practice
 
 매일프로그래밍에서 제공하는 코딩테스트
+
+https://mailprogramming.com/
